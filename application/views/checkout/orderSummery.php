8d4c40922a8bbc27ae242b293d197a9ffb868708
enalamin <enalamin@gmail.com>
2015-10-26 17:58:20 +0600

    some changes on checkout pages

--- application/views/checkout/orderSummery.php
+++ application/views/checkout/orderSummery.php
@@ -43,175 +43,203 @@
         </div>
 
     </div>
-    <div class="row">
-        <br>
-        </div>
-
-          <div class="row cart-style">
-                <div class="col-md-3">
-                    ITEM
-                </div>
-                <div class="col-md-1">
-                    QTY
-                </div>
-                <div class="col-md-1">
-                    UNIT PRICE
-                </div>
-              <div class="col-md-2">
-                  SUBTOTAL
-              </div>
-                <div class="col-md-2">
-                    <div>DELIVERY DATE</div>
-                </div>
-                <div class="col-md-2">
-                    <div>FREQUENCY</div>
-                </div>
-              <div class="col-md-1">
-                  <div>REMOVE</div>
-              </div>
-          </div>
-
-            <?php
-            if(!empty($itemDetails))
-            {
-            $subtotal = 0;
-            $tax = 0;
-
-            foreach($itemDetails as $items ){ ?>
-                <div class="row vertical-align" style="margin: 5px 0;border-top:1px solid #fbb03b;">
-
-                    <div class="col-md-2 text-left">
-                        <div class="row">
-                            <div class="col-md-12"><?php echo $items['manufacturer_name']; ?></div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-12"><b><?php echo $items['product_name']; ?></b></div>
-                        </div>
-
-                    </div>
-                    <div class="col-md-1">
-                        <div><input type="text" class="cart-qty" name="number_qty_<?php echo $items['product_id']; ?>" id="number_qty_<?php echo $items['product_id']; ?>; ?>" value="<?php echo $items['qty'];?>"></div>
-
-                    </div>
-
-                    <div class="col-md-2">
-                        <div class="font-bold"> $ <?php echo $items['price']; ?></div>
-                    </div>
-                    <div class="col-md-2">
-                        <div class="font-bold"> $ <?php echo $items['price'] * $items['qty']; ?></div>
-                    </div>
-
-                    <div class="col-md-2">
-                        <div>
-                            <select name="deliveryDate_<?php echo $items['item_id']?>" id="deliveryDate_<?php echo $items['item_id']?>">
-                                <?php
-                                foreach($deliveryDayList as $deliveryDay ){
-                                    ?>
-                                    <option value='<?php echo $deliveryDay["dateValue"];?>' <?php echo $deliveryDay["dateValue"]==$items['next_delivery']?"Selected":"";?> ><?php echo $deliveryDay["dateDisplay"];?></option>
-                                    <?php
-                                }
-                                if($items['frequency_id']==0){
-                                    ?>
-                                    <option value='0000-00-00' >Cancel Order</option>
-                                    <?php
-                                }
-                                ?>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="col-md-2">
-                        <div>
-                            <select name="frequency_<?php echo $items['item_id']?>" id="frequency_<?php echo $items['item_id']?>">
-                                <?php
-                                foreach($frequencyList as $frequency ){
-                                    ?>
-                                    <option value='<?php echo $frequency["frequency_id"];?>' <?php echo $frequency["frequency_id"]==$items['frequency_id']?"Selected":"";?> ><?php echo $frequency["frequency"];?></option>
-                                    <?php
-                                }
-                                if($items['frequency_id']==0){
-                                    ?>
-                                    <option value='0000-00-00' >Cancel Order</option>
-                                    <?php
-                                }
-                                ?>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="col-xs-4 col-md-1">
-                        <div style="background-color: #fbb03b;border-radius: 6px;color: #ffffff;">x</div>
-                    </div>
-
-                </div>
-                <script type="text/javascript">
-                    loadCustomeCombo(<?php echo $items['item_id'] ;?>);
-                    loadDeliveryDate(<?php echo $items['item_id'] ;?>);
-                </script>
-
     <?php
-                $subtotal += $items['price'];
-            }
-                $tax = $subtotal * 0.0740;
-                $total = $subtotal+$tax;
-            }?>
+    if(!empty($itemDetails)) {
+        $subtotal = 0;
+        $tax = 0;
+        ?>
+        <div class="row">
+            <div class="customer-info text-center">
+                This products will be delivered on <?php
+                $tmpDate = strtotime($itemDetails[0]['next_delivery']);
+                $currentTmpDate = strtotime(date("Y-m-d"));
+                $dayRemaining = $tmpDate - $currentTmpDate;
+                $dno = round($dayRemaining / 86400 )==1?round($dayRemaining / 86400 )." Day":round($dayRemaining / 86400 )." Days";
 
+                $sDeliveryDate_day = date("l, m/d/Y",$tmpDate)." (".$dno.")"; //date("m-d-Y (l)",$tmpDate);
 
-
-    <div class="row">
-        <div class="col-md-12 text-left">
-            <div class="card-info">
-                New Credit Card Info
+                echo $sDeliveryDate_day ;?>
             </div>
         </div>
-    </div>
+
+        <div class="row" style="border-bottom:1px solid #fbb03b;">
+            <div class="col-md-3">
+                ITEM
+            </div>
+            <div class="col-md-1">
+                QTY
+            </div>
+            <div class="col-md-1">
+                UNIT PRICE
+            </div>
+            <div class="col-md-2">
+                SUBTOTAL
+            </div>
+            <div class="col-md-2">
+                DELIVERY DATE
+            </div>
+            <div class="col-md-2">
+                FREQUENCY
+            </div>
+            <div class="col-md-1">
+                REMOVE
+            </div>
+        </div>
+
+    <?php
+
+    foreach ($itemDetails as $items){ ?>
+        <div class="row vertical-align" style="border-bottom:1px solid #fbb03b;">
+
+            <div class="col-md-3 ">
+                <div class="row">
+                    <div class="col-md-12 text-left">
+                        <div><?php echo $items['manufacturer_name']; ?></div>
+
+                        <b><?php echo $items['product_name']; ?></b>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-md-1">
+                <input type="text" class="cart-qty" name="number_qty_<?php echo $items['product_id']; ?>"
+                       id="number_qty_<?php echo $items['product_id']; ?>; ?>" value="<?php echo $items['qty']; ?>">
+
+            </div>
+
+            <div class="col-md-2">
+                <span class="font-bold"> $ <?php echo $items['price']; ?></span>
+            </div>
+            <div class="col-md-2">
+                <span class="font-bold"> $ <?php echo $items['price'] * $items['qty']; ?></span>
+            </div>
+
+            <div class="col-md-2">
+
+                <select name="deliveryDate_<?php echo $items['item_id'] ?>" id="deliveryDate_<?php echo $items['item_id'] ?>">
+                    <?php
+                    foreach ($deliveryDayList as $deliveryDay) {
+                    ?>
+                        <option
+                            value='<?php echo $deliveryDay["dateValue"]; ?>' <?php echo $deliveryDay["dateValue"] == $items['next_delivery'] ? "Selected" : ""; ?> ><?php echo $deliveryDay["dateDisplay"]; ?></option>
+                    <?php
+                    }
+                    if ($items['frequency_id'] == 0) {
+                    ?>
+                        <option value='0000-00-00'>Cancel Order</option>
+                        <?php
+                    }
+                    ?>
+                </select>
+
+            </div>
+            <div class="col-md-2">
+                <select name="frequency_<?php echo $items['item_id'] ?>" id="frequency_<?php echo $items['item_id'] ?>">
+                    <?php
+                    foreach ($frequencyList as $frequency) {
+                        ?>
+                        <option
+                            value='<?php echo $frequency["frequency_id"]; ?>' <?php echo $frequency["frequency_id"] == $items['frequency_id'] ? "Selected" : ""; ?> ><?php echo $frequency["frequency"]; ?></option>
+                        <?php
+                    }
+                    if ($items['frequency_id'] == 0) {
+                        ?>
+                        <option value='0000-00-00'>Cancel Order</option>
+                        <?php
+                    }
+                    ?>
+                </select>
+
+            </div>
+            <div class="col-md-1">
+                <div style="background-color: #fbb03b;border-radius: 6px;color: #ffffff;">x</div>
+            </div>
+
+        </div>
+        <script type="text/javascript">
+            loadCustomeCombo(<?php echo $items['item_id'] ;?>);
+            loadDeliveryDate(<?php echo $items['item_id'] ;?>);
+        </script>
+
+    <?php
+    $subtotal += $items['price'];
+    }
+    $tax = $subtotal * 0.0740;
+    $total = $subtotal + $tax;
+    ?>
+
+
+        <div class="row">
+            <div class="col-md-12 text-left">
+                <div class="card-info">
+                    New Credit Card Info
+                </div>
+            </div>
+        </div>
         <div class="row">
             <div class="col-md-2 text-left">
                 <span>
                     <input type="radio" name="cardName" id="cardName" value="Visa"/> Visa <br>
-                    <input type="radio" name="cardName" id="cardName" value="Master Card" /> Master <br>
-                    <input type="radio" name="cardName" id="cardName" value="Master Card" /> American Express <br>
-                    <input type="radio" name="cardName" id="cardName" value="Master Card" /> Discover <br>
+                    <input type="radio" name="cardName" id="cardName" value="Master Card"/> Master <br>
+                    <input type="radio" name="cardName" id="cardName" value="Master Card"/> American Express <br>
+                    <input type="radio" name="cardName" id="cardName" value="Master Card"/> Discover <br>
                 </span>
-             </div>
+            </div>
             <div class="col-md-2">
                 <span>
-                    <input type="text" class="card-info-box" name="customerCardName" id="customerCardName" value="" /></br>
-                    <input type="text" class="card-info-box" name="customerCardName" id="customerCardName" value="" /></br>
-                    <input type="text" class="card-info-box" name="customerCardName" id="customerCardName" value="" /></br>
-                    <input type="text" class="card-info-box" name="customerCardName" id="customerCardName" value="" /></br>
+                    <input type="text" class="card-info-box" name="customerCardName" id="customerCardName"
+                           value=""/></br>
+                    <input type="text" class="card-info-box" name="customerCardName" id="customerCardName"
+                           value=""/></br>
+                    <input type="text" class="card-info-box" name="customerCardName" id="customerCardName"
+                           value=""/></br>
+                    <input type="text" class="card-info-box" name="customerCardName" id="customerCardName"
+                           value=""/></br>
                 </span>
             </div>
             <div class="col-md-2">
                 <img class="img-responsive" src="http://localhost/fetch/img/account_fish.jpg">
             </div>
             <div class="col-md-4 font-bold">
-                <div style=""><div style="float: left;margin: 8px 22px;">Enter Discount Code </div> <input type="text" class="credit-info-box" name="customerCardName" id="customerCardName" value="" /></div></br>
-                <div style=""><div style="width:50%;float: left;margin-top: -10px;"> Available <br> Gift Certificate Credit </div> <input type="text" class="credit-info-box" name="customerCardName" id="customerCardName" value="" /></div></br>
-                <div style=""> <div style="width:50%;float: left;margin-top: -10px;">Available <br> Account Credit</div>  <input type="text" class="credit-info-box" name="customerCardName" id="customerCardName" value="" /></div></br>
+                <div style="">
+                    <div style="float: left;margin: 8px 22px;">Enter Discount Code</div>
+                    <input type="text" class="credit-info-box" name="customerCardName" id="customerCardName" value=""/>
+                </div>
+                </br>
+                <div style="">
+                    <div style="width:50%;float: left;margin-top: -10px;"> Available <br> Gift Certificate Credit</div>
+                    <input type="text" class="credit-info-box" name="customerCardName" id="customerCardName" value=""/>
+                </div>
+                </br>
+                <div style="">
+                    <div style="width:50%;float: left;margin-top: -10px;">Available <br> Account Credit</div>
+                    <input type="text" class="credit-info-box" name="customerCardName" id="customerCardName" value=""/>
+                </div>
+                </br>
             </div>
             <div class="col-md-2 text-left">
-                <button style="width:100%" name="total" value="" class="btn-add">TOTAL </button><br>
+                <button style="width:100%" name="total" value="" class="btn-add">TOTAL</button>
+                <br>
+
                 <div class="cart-summary ">
-                    <span class="cart-text"> SUBTOTAL $<?php echo $subtotal;?> </span><br>
-                    <span class="cart-text">  TAX  $<?php echo number_format($tax,2);?> </span><br>
+                    <span class="cart-text"> SUBTOTAL $<?php echo $subtotal; ?> </span><br>
+                    <span class="cart-text">  TAX  $<?php echo number_format($tax, 2); ?> </span><br>
                     <span class="cart-text"> DELIVERY FEE </span><br>
-                    <span class="cart-text text-color"> TOTAL $<?php echo number_format($total,2);?> </span>
+                    <span class="cart-text text-color"> TOTAL $<?php echo number_format($total, 2); ?> </span>
                 </div>
-                <button type="button" style="width:100%" name="checkout" id="checkout" value="" class="btn-add checkout-btn" onclick="window.location='<?php echo base_url()?>checkout/orderSummery'">CONFIRM</button><br>
+                <button type="button" style="width:100%" name="checkout" id="checkout" value=""
+                        class="btn-add checkout-btn"
+                        onclick="window.location='<?php echo base_url() ?>checkout/orderSummery'">CONFIRM
+                </button>
+                <br>
 
             </div>
         </div>
+    <?php
+    }
+    ?>
 
 
-                <!--div class="text-right" style="padding-left:50px;">
-                    <button style="width:25%" name="total" value="" class="btn-add">TOTAL </button><br>
-                    <div class="cart-summary text-right">
-                        <span class="cart-text"> SUBTOTAL $<?php echo $subtotal;?> </span><br>
-                        <span class="cart-text">  TAX  $<?php echo number_format($tax,2);?> </span><br>
-                        <span class="cart-text"> DELIVERY FEE </span><br>
-                        <span class="cart-text text-color"> TOTAL $<?php echo number_format($total,2);?> </span>
-                    </div>
-                    <button type="button" style="width:25%" name="checkout" id="checkout" value="" class="btn-add checkout-btn" onclick="window.location='<?php echo base_url()?>checkout/orderSummery'">CHECKOUT</button><br>
-                </div-->
 </div>
 <?php
 
